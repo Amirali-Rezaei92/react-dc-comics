5e856119740363b87621b92dcc0c02be44ef5502
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-04-28 08:30:21 +0200

    feat: refactor BlueBar with CSS Modules and add global container support

--- src/components/BlueBar.jsx
+++ src/components/BlueBar.jsx
@@ -1,14 +1,29 @@
-
-import card-bluebar from './card-bluebar';
+import styles from './BlueBar.module.css';
 
 function BlueBar() {
+
+    const items = [
+        { img: "/img/buy-comics-digital-comics.png", text: "DIGITAL COMICS" },
+        { img: "/img/buy-comics-merchandise.png", text: "DC MERCHANDISE" },
+        { img: "/img/buy-comics-subscriptions.png", text: "SUBSCRIPTIONS" },
+        { img: "/img/buy-comics-shop-locator.png", text: "COMIC SHOP LOCATOR" },
+        { img: "/img/buy-dc-power-visa.svg", text: "DC POWER VISA" }
+    ];
+
     return (
-    <div style={{ backgroundColor: 'rgb(47, 164, 215)', color: 'white', height: '140px', width: '100%' }} className="flex p-3 justify-content-around ">
-        <div className="flex justify-content-start align-items-center gap-2" > 
-            <img src="/img/buy-comics-digital-comics.png" alt="" style={{width:'50px'}}/>
-        <span>DIGITAL COMICS</span>
+        <div className={styles.bluebar}>
+            <div className="container">
+                <div className={styles.itemsWrapper}>
+                    {items.map((item, index) => (
+                        <div key={index} className={styles.bluebarItem}>
+                            <img src={item.img} alt={item.text} />
+                            <span>{item.text}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
-        
-    </div>)
+    );
 }
+
 export default BlueBar;
--- src/components/card-bluebar.jsx
+++
@@ -1,5 +0,0 @@
-
-function cardBluebar(){
-    
-}
-export default cardBluebar;
